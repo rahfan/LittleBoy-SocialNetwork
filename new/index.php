c2a153e64af0c1e342f90d20f01f428267d4b8b8
Muhammad Arfan Asri <arfanasri@gmail.com>
2015-10-09 07:05:45 +0800

    Update V0.0.1
    Versi Alpha paling pertama

---
+++ new/index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<title>
+			Social Network
+		</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="script/w3.css">
+		<link rel="stylesheet" href="script/w3-theme-blue.css">
+	</head>
+	<body>
+		<header class='w3-container w3-theme'>
+			<h1>Social Network</h1>
+		</header>
+		<div class='w3-row'>
+			<div class='w3-col w3-card-4 m4 l3'>
+				Arfan asri
+			</div>
+			<div class='w3-col w3-card-4 m8 l9'>
+				<?PHP
+					if(isset($_GET['halaman'])) {
+						$halaman=$_GET['halaman'];
+						$halaman .= '.php';
+						include ($halaman);
+					} else {
+						include ('halamanutama.php');
+					}		
+				?>
+			</div>
+		</div>
+	</body>
+</html>
--- new/user/daftar.php
+++ new/user/daftar.php
@@ -8,6 +8,15 @@
 	echo $tanggallahir;
 
 ?>
+<form class='w3-container w3-card-4'>
+	<div class='w3-group'>
+		<input class='w3-input' name='username' type='text' style='width:95%' required>
+		<label class='w3-label'>Username</label>
+	</div>
+	<div class='w3-group'>
+		<input class='w3-input' name='nama' type='text' style='width:95%' required>
+		<label class='w3-label'>Nama Lengkap</label>
+	</div>
 	<div class='w3-group'>
 		<input class='w3-input' name='tanggallahir' type='date' value='<?PHP echo $tanggallahir;?>' style='width:95%' required>
 		<label class='w3-label'>Tanggal Lahir</label>
@@ -16,3 +25,4 @@
 		<br><input class='w3-text' name='foto' type='file' style='width:95%' required>
 		<label class='w3-label'>Foto</label>
 	</div>
+</form>
